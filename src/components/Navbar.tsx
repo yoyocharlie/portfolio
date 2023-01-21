c0b0bc5ad71d77f370580f081288309e021e84bd
Jacob Pixler <drivrartist@gmail.com>
2023-01-21 15:59:12 -0600

    updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 import React, { Dispatch, SetStateAction } from 'react'
 
-interface Sidebar{
+    interface Sidebar{
     showSidebar: boolean,
     setShowSidebar: Dispatch<SetStateAction<boolean>>,
 }
 
-const Navbar: React.FC<Sidebar> = ({showSidebar, setShowSidebar}) => {
+    const Navbar: React.FC<Sidebar> = ({showSidebar, setShowSidebar}) => {
     console.log(showSidebar)
+    const buttonClass = "bg-amber rounded text-2xl p-2 mb-5 flex flex-row items-center gap-5 mx-4 hover:cursor-pointer"
+
+
   return (
     <div className='py-2 px-10 flex shadow-xl items-center'>
         <div className='mr-auto'>
-            <span className='text-3xl text-jet mr-2 font-oswald font-bold'>Jacob</span>
-            <span className='text-3xl text-amber font-oswald font-bold mr-auto'>Pixler</span>
+            <span className='text-4xl text-jet mr-2 font-oswald font-bold'>Jacob</span>
+            <span className='text-4xl text-amber font-oswald font-bold mr-auto'>Pixler</span>
         </div>
         <div>
             <button onClick={() => setShowSidebar(!showSidebar)} className='text-amber md:hidden pt-2'>
@@ -19,6 +22,32 @@ const Navbar: React.FC<Sidebar> = ({showSidebar, setShowSidebar}) => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </button>
+        </div>
+        <div className='flex'>
+        <button
+            className={`bg-white text-amber border border-amber mt-2 ${buttonClass}`}
+            onClick={() => setShowSidebar(!showSidebar)}
+        >
+            <h3>Close</h3>
+        </button>
+        <a className={`${buttonClass} mt-8`}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+            </svg>
+            Projects
+        </a>
+        <a className={buttonClass}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+            </svg>
+            About
+        </a>
+        <a className={buttonClass}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+            </svg>
+            Contact
+        </a>
         </div>
     </div>
   )
